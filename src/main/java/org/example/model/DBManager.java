54ebece972b8a5c90790f1a7df2c94a3e54b8cc1
abdelrahman salama <abdelrahmansalamaaaa@gmail.com>
2025-05-09 18:05:06 +0300

    final edit :)

--- src/main/java/org/example/model/DBManager.java
+++ src/main/java/org/example/model/DBManager.java
@@ -139,7 +139,7 @@ public class DBManager implements AutoCloseable {
     }
 
     public Item idSearch(String username){
-        String query = "SELECT * FROM MT WHERE username = ?";
+        String query = "SELECT * FROM MT WHERE USER = ?";
         try (Connection conn = DriverManager.getConnection(URL);
              PreparedStatement pstmt = conn.prepareStatement(query)) {
 

52a1c0619a2be7acb85453381d4a76004a1f92fb
Umbriumm <darkshadow22322@gmail.com>
2025-05-09 03:50:53 +0300

    - Database operations are now functional and can look up entries using the ID - Separated database load / refresh logic from the frontend to the backend (DBManager) for a cleaner structure - Database view now refreshes properly after every operation done - Moved the Item class to the model and modified it to process IDs - Fixed some SQL statements and other bugs

--- src/main/java/org/example/controller/mainController.java
+++ src/main/java/org/example/controller/mainController.java
@@ -2,15 +2,19 @@ package org.example.controller;
 
 import org.example.model.CryptographyException;
 import org.example.model.DBManager;
+import org.example.model.Item;
 
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class mainController {
 
     DBManager DBM;
     private Connection connection;
 
-    public mainController(String masterpassword){
+    public mainController(String masterpassword) throws CryptographyException, NoSuchAlgorithmException {
         DBM = new DBManager(masterpassword);
 
     }
@@ -21,12 +25,16 @@ public class mainController {
         DBM.insertRecord(Site, Username, Password, Notes);
     }
 
-    public static void editRecord(int id){
-        // edit method
+    public void updateRecord(int id, String Site, String Username, String Password, String Notes) throws CryptographyException {
+        DBM.updateRecord(id, Site, Username, Password, Notes);
     }
 
-    public static void deleteRecord(int id){
-        // delete method
+    public void deleteRecord(int id) throws SQLException {
+        DBM.deleteRecord(id);
+    }
+
+    public List<Item> loadAllItems(){
+        return DBM.getAllItems();
     }
 
     public Connection getConnection() {
--- src/main/java/org/example/model/Item.java
+++ src/main/java/org/example/model/Item.java
@@ -1,20 +1,32 @@
-package org.example.view;
+package org.example.model;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Item {
+    private int ID;
     private final StringProperty site;
     private final StringProperty username;
     private final StringProperty password;
     private final StringProperty notes;
 
-    public Item(String site, String username, String password, String notes) {
+    public Item(int id, String site, String username, String password, String notes) {
+        this.ID = id;
         this.site = new SimpleStringProperty(site);
         this.username = new SimpleStringProperty(username);
         this.password = new SimpleStringProperty(password);
         this.notes = new SimpleStringProperty(notes);
     }
+
+    public Item(String site, String username, String password, String notes) {
+        this.ID = -1;
+        this.site = new SimpleStringProperty(site);
+        this.username = new SimpleStringProperty(username);
+        this.password = new SimpleStringProperty(password);
+        this.notes = new SimpleStringProperty(notes);
+    }
+
+    public int getID() { return ID; }
 
     public String getSite() { return site.get(); }
     public void setSite(String value) { site.set(value); }
